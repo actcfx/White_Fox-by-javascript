1a1cb1cb08574fedb56c988e19bb439a9e714050
北極狐 <cheung950624@gmail.com>
2022-06-28 02:59:56 +0800

    add something

--- bot.py
+++ bot.py
@@ -6,15 +6,19 @@ bot = commands.Bot('/')
 
 @bot.event
 async def on_ready():
-    print(">> Bot_Test is on line <<")
+    print('The Bot is on line...')
     
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_all_channel(991020606587822181)
-    await channel.send(f"{member} join!")
+    channel = bot.get_channel(991020606587822181)
+    await channel.send(f'{member} join!')
     
 @bot.event
 async def on_member_remove(member):
-    print(f"{member} leave")
+    print(f'{member} leave')
+    
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
 
 bot.run("OTkxMDQzMjY2Mjk0MjA2NDk0.Ga9eTE.Raix_2C-C7dzkU4FNWBmTNfGKHZr9d7M8ZJUNw")
